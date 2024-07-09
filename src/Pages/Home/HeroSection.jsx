07fd618a51483bc072611e7849bffe9543a6a492
Suyash1231 <154902233+Suyash1231@users.noreply.github.com>
2024-07-09 16:00:39 +0530

    added button sytling in app.css and hithubbtn too

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,5 +1,26 @@
 export default function HeroSection() {
     return (
-        <h1>HeroSection</h1>
+        <section id = "heroSection" className="hero--section">
+            <div className="hero--sectionh--content--box">
+                <div className="hero--section-content">
+                    <p className="section--title"> Hey, I am Suyash</p>
+                    <h1 className="hero--section--title">
+                        <span className="hero--section-title--color">
+                            Full Stack
+                        </span>{" "}
+                        <br />
+                        Developer
+                    </h1>
+                    <p className="hero--section-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    <br /> Lorem, ipsum dolor sit amet consectetur adipisicing elit.    
+                    </p>
+                </div>
+                <button className="btn btn-primary">Get in Touch</button>
+            </div>
+            <div className="hero--section--img">
+                <img src="./img/hero_img.png" alt="Hero Section" />
+            </div>
+        </section>
     )
 }
--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -6,4 +6,5 @@ export default function Homescreen() {
             <HeroSection />
         </>
     )
+    
 }
