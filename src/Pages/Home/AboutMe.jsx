2b04a182ac4ed69cd9aec7b6b5549b99fccf21b6
Suyash1231 <154902233+Suyash1231@users.noreply.github.com>
2024-07-15 16:30:19 +0530

    Started aBOUT ME Section and finished Skills styles

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,25 @@
+export default function AboutMe() {
+  return (
+    <section id = "AboutMe" className="about--section" >
+        <div className="about--section--img">
+            <img src="./img/about-me.png" alt="AboutMe"/>
+        </div>
+        <div className="hero--section--content--box about--section--box">
+            <div className="hero--section--content">
+                <p className="section--title">
+                    About
+                </p>
+                <h1 className="skills-section--heading">
+                    About Me
+                </h1>
+                <p className="hero--section-description">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, debitis fugiat odit nostrum molestias sed quis cumque iste dolores quam a at quisquam, harum ipsa et quaerat, sint modi natus.
+                </p>
+                <p className="hero--section-description">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, debitis fugiat odit nostrum molestias sed quis cumque iste dolores quam a at quisquam, harum ipsa et quaerat, sint modi natus.
+                </p>                
+            </div>
+        </div>
+    </section>
+  );
+}
