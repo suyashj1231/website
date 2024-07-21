74ccd00dcb427e498320255fc4c3fc0257b5f9c8
Suyash1231 <154902233+Suyash1231@users.noreply.github.com>
2024-07-21 16:35:33 +0530

    disables testimonials and starting about me

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -11,7 +11,7 @@ export default function Homescreen() {
             <MySkills />
             <AboutMe />
             <MyPortfolio />
-            <Testimonial />
+            {/* <Testimonial /> */}
         </>
     )
     
